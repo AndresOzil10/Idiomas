79fa4ebec975eb9b5241d10f8a36017b82806f28
AndresOzil10 <andresref29@gmail.com>
2025-04-24 10:11:15 -0600

    update colors bar graphycs

--- src/widgets/bargraphycs.jsx
+++ src/widgets/bargraphycs.jsx
@@ -1,3 +1,4 @@
+import { axisClasses } from '@mui/x-charts';
 import { BarChart } from '@mui/x-charts/BarChart';
 
 const BarGraphycs = () => { 
@@ -5,8 +6,24 @@ const BarGraphycs = () => {
     return <>
         <div className='w-1/2'>
             <BarChart
+            sx={{
+                [`.${axisClasses.root}`] : {
+                    [`.${axisClasses.tick}, .${axisClasses.line}`]: {
+                        stroke: '#FFFFFF',
+                        strokeWidth: 1,
+                      },   
+                    [`.${axisClasses.tickLabel}`]: {
+                        fill: 'white',
+
+                    },
+                }
+            }}
             xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
-            series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+            series={[
+                { data: [4, 3, 5], color: '#FF5733' },
+                { data: [1, 6, 3], color: '#33FF57' },
+                { data: [2, 5, 6], color: '#3357FF' }
+            ]}
             width={isSmallScreen ? 650 : 800}
             height={isSmallScreen ? 300 : 400}
             />
--- src/widgets/graphycs.jsx
+++ src/widgets/graphycs.jsx
@@ -1,3 +1,4 @@
+import { axisClasses } from '@mui/x-charts';
 import { PieChart } from '@mui/x-charts/PieChart';
 
 const Graphycs = () => { 
@@ -6,17 +7,25 @@ const Graphycs = () => {
     return (
         <div className='w-1/2'>
             <PieChart
+                sx={{
+                    [`.${axisClasses.root}`] : {
+                        [`.${axisClasses.tick}, .${axisClasses.tickLabel}`]: {
+                            stroke: 'white',
+                            fill: 'white',
+                    }}
+                }}
                 series={[
                     {
-                        data: [   
-                            { id: 0, value: 10, label: 'series A' },
-                            { id: 1, value: 15, label: 'series   B' },
-                            { id: 2, value: 20, label: 'series C' }, 
+                        data: [
+                            { id: 0, value: 10, label: 'series A', color: '#FF5733' },
+                            { id: 1, value: 15, label: 'series B', color: '#33FF57' },
+                            { id: 2, value: 20, label: 'series C', color: '#3357FF' },
                         ],
                     },
                 ]}
                 width={isSmallScreen ? 650 : 800}
                 height={isSmallScreen ? 300 : 400}
+                
             />
         </div>
     );
